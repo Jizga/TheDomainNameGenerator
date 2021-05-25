ece277d2b65089a38541ff03e00dcdf595b1fcc6
Jizga <jizga93@gmail.com>
2021-05-25 09:24:58 +0000

    A code more dinamic

--- src/app.js
+++ src/app.js
@@ -20,7 +20,11 @@ const generateDomains = (arr1, arr2, arr3, arr4) => {
     )
   );
 
-  return domainList.map(
+  return domainList;
+};
+
+const formGenerateDomains = arr => {
+  return arr.map(
     str =>
       //   Hay que hacer esta parte más dinámica
       `<li>
@@ -30,33 +34,31 @@ const generateDomains = (arr1, arr2, arr3, arr4) => {
   );
 };
 
-const hideShowRegards = () => {
-  //Uso clases e id ya que las clases van a variar
-  let regards = document.querySelector("#regards");
-  //Para poder trabajar con las clases del DOM como si fuera un array
-  const classes = [...regards.classList];
-
-  if (classes.includes("d-flex")) {
-    regards.classList.remove("d-flex");
-    regards.classList.add("d-none");
-  } else {
-    regards.classList.remove("d-none");
-    regards.classList.add("d-flex");
-    document.querySelector("#domain").innerHTML =
-      "You need to click on the button to show the domains list";
-  }
-};
+const formSplitDomainsList = splitArr =>
+  `<div class = "col-6 p-0"> ${splitArr.join("")} </div>`;
 
 const splitDomainsList = arr => {
   let firstPartArr = arr.slice(0, arr.length / 2);
   let secondPartArr = arr.slice(arr.length / 2, arr.length);
 
-  return `<div class = "col-6 p-0">
-            ${firstPartArr}
-          </div>
-          <div class = "col-6 p-0">
-            ${secondPartArr}
-          </div>`;
+  return (
+    formSplitDomainsList(firstPartArr) + formSplitDomainsList(secondPartArr)
+  );
+};
+
+const hideShowRegards = (domIdHideShow, domIdGenerate) => {
+  //Para poder trabajar con las clases del DOM como si fuera un array
+  const classes = [...domIdHideShow.classList];
+
+  if (classes.includes("d-flex")) {
+    domIdHideShow.classList.remove("d-flex");
+    domIdHideShow.classList.add("d-none");
+  } else {
+    domIdHideShow.classList.remove("d-none");
+    domIdHideShow.classList.add("d-flex");
+    domIdGenerate.innerHTML =
+      "You need to click on the button to show the domains list";
+  }
 };
 
 window.onload = () => {
@@ -66,9 +68,12 @@ window.onload = () => {
     document.querySelector(
       "#domain"
     ).innerHTML = `<div class = "row p-2">${splitDomainsList(
-      generateDomains(pronoun, adj, noun, extensions).join(" ")
+      formGenerateDomains(generateDomains(pronoun, adj, noun, extensions))
     )}</div>`;
 
-    hideShowRegards();
+    let regards = document.querySelector("#regards");
+    let initText = document.querySelector("#domain");
+
+    hideShowRegards(regards, initText);
   });
 };
